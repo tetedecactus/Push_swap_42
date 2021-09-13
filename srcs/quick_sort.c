11be91e18797cdde8ae913501ab4f295cbc52044
Olivier Labrecque-lacasse <olabrecq@c2r4p3.42quebec.com>
2021-09-13 13:09:26 -0400

    just start quicksort

--- srcs/quick_sort.c
+++ srcs/quick_sort.c
@@ -6,8 +6,16 @@
 /*   By: olabrecq <olabrecq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/13 11:46:41 by olabrecq          #+#    #+#             */
-/*   Updated: 2021/09/13 11:46:53 by olabrecq         ###   ########.fr       */
+/*   Updated: 2021/09/13 13:06:25 by olabrecq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
+
+void quick_sort(int *arr, int stack_size)
+{
+    int i;
+    int left;
+    int right;
+    int pivot;
+}
